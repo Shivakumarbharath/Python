44da98df348673014b888bed8a2372966706af28
Shivakumarbharath <shivakumar.bharath@gmail.com>
2020-05-01 23:54:11 +0530

    Using Sqlite3 storing data using sqlite3 database

---
+++ PYTHONprograms/DataBaseSqlite3/InsertingDataIntoDB.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+conn=sqlite3.connect("Our_data.db")
+c=conn.execute("""INSERT INTO Employee_records(ID,name,dept,STARS)
+VALUES(2,'SUJITH','CSE',10)
+
+
+""")
+'''
+method 2
+c=conn.execute("""INSERT INTO Employee_records(ID,name,dept,STARS)
+VALUES(?,?,?,?)
+
+
+""",id,name,dept,stars)
+
+
+'''
+conn.commit()
+conn.close()
---
+++ PYTHONprograms/DataBaseSqlite3/UpdatingARecord.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+conn=sqlite3.connect("Our_data.db")
+c=conn.cursor()
+c.execute(""" UPDATE Employee_records set name='S.Bharath' WHERE ID= {}""".format(1))
+rec=c.execute("""SELECT * FROM Employee_records""")
+print(rec.fetchall())
+conn.commit()
+conn.close()
